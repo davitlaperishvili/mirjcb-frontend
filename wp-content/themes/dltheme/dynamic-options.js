f6a98cd21ef6478c82de520765ec1a019b708932
dlaperishvili <davitlaperishvili@gmail.com>
2023-08-02 01:16:02 +0400

    search dynamic options are added

---
+++ wp-content/themes/dltheme/dynamic-options.js
@@ -0,0 +1,85 @@
+const productSearchURL =
+  "https://mirjcb.ru/index.php?route=product/search&fsearch=true&search=";
+const catSearchURL =
+  "https://mirjcb.ru/index.php?route=product/catsearch&fsearch=true&search=";
+
+async function getSearchResult(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+}
+function createOptionsHTML(dataArr) {
+  const options = dataArr
+    .map((option) => {
+      const { href, name } = option;
+      return `
+    <li>
+      <a href="${href}" class="option_item">${name}</a>
+    </li>
+    `;
+    })
+    .join(" ");
+  if (!options) return "";
+  return `<ul>${options}</ul>`;
+}
+
+function loadOptions(parentElem, selector, content) {
+  if (content) {
+    parentElem.querySelector(selector).innerHTML = content;
+    parentElem.querySelector(selector).closest(".options_group").style.display =
+      "block";
+  } else {
+    parentElem.querySelector(selector).closest(".options_group").style.display =
+      "none";
+  }
+}
+
+const dynamicOptionsInputs = document.querySelectorAll(
+  ".dynamic_options_input"
+);
+let timer;
+dynamicOptionsInputs.forEach((input) => {
+  input.addEventListener("input", function (e) {
+    try {
+      clearTimeout(timer);
+      timer = setTimeout(async function () {
+        const value = e.target.value;
+        const parent = e.target.parentElement;
+        const productSearch = await getSearchResult(
+          `${productSearchURL}${value}`
+        );
+        const productSearchHTML = createOptionsHTML(productSearch);
+
+        const productCatSearch = await getSearchResult(
+          `${catSearchURL}${value}`
+        );
+        const catSearchHTML = createOptionsHTML(productCatSearch);
+
+        loadOptions(parent, "#categories", catSearchHTML);
+        loadOptions(parent, "#products", productSearchHTML);
+
+        parent.classList.add("is-active");
+      }, 400); // Delay in milliseconds
+    } catch (e) {}
+  });
+});
+
+document.querySelectorAll(".close_dynamic_options").forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.target.closest("#search").classList.remove("is-active");
+  });
+});
+
+window.addEventListener("click", function (e) {
+  const target = e.target;
+  if (!e.target.closest("#search")) {
+    document.querySelectorAll("#search").forEach((search) => {
+      search.classList.remove("is-active");
+    });
+  }
+});
--- wp-content/themes/dltheme/single.php
+++ wp-content/themes/dltheme/single.php
@@ -1,5 +1,74 @@
 <?php get_header(); ?>
 
-
+<form action="" role="search" method="get" action="https://mirjcb.ru/">
+  <div class="search_input">
+    <input type="text" name="search" oninput="setSearch(this)" value="" placeholder="Поиск среди 950 тысяч артикулов с актуальными наличием и ценами">
+    <div class="dynamic_options">
+      <div class="options_group">
+        <div class="group_title">Категории</div>
+        <div class="group_items" id="categories">
+          <ul>
+            <li>
+              <a href="#">Пальцы и втулки</a>
+            </li>
+            <li>
+              <a href="#">Пальцы и втулки</a>
+            </li>
+            <li>
+              <a href="#">Пальцы и втулки</a>
+            </li>
+            <li>
+              <a href="#">Пальцы и втулки</a>
+            </li>
+            <li>
+              <a href="#">Пальцы и втулки</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="options_group">
+        <div class="group_title">Товары</div>
+        <div class="group_items" id="products">
+          <ul>
+            <li>
+              <a href="#">1208/0015 Втулка штока JCB</a>
+            </li>
+            <li>
+              <a href="#">808/00296 Втулка передней стрелы</a>
+            </li>
+            <li>
+              <a href="#">808/00388 Втулка каретки</a>
+            </li>
+            <li>
+              <a href="#">809/00126 Втулка задней стрелы</a>
+            </li>
+            <li>
+              <a href="#">809/00126 Втулка задней стрелы</a>
+            </li>
+            <li>
+              <a href="#">809/00126 Втулка задней стрелы</a>
+            </li>
+            <li>
+              <a href="#">809/00126 Втулка задней стрелы</a>
+            </li>
+            <li>
+              <a href="#">809/00126 Втулка задней стрелы</a>
+            </li>
+            <li>
+              <a href="#">809/00126 Втулка задней стрелы</a>
+            </li>
+            <li>
+              <a href="#">809/00126 Втулка задней стрелы</a>
+            </li>
+            <li>
+              <a href="#">809/00126 Втулка задней стрелы</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <button type="button">Искать</button>
+</form>
 
 <?php get_footer(); ?>
